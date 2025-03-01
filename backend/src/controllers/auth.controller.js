14e0dd484d9f7d65c58073cf765908c71693c00d
Rohit Thakur <kumarrohit58590@gmail.com>
2025-03-01 13:00:25 +0530

    implement user authentication with signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,57 @@
-export const signup = (req,res)=>{
-    res.send("signup route")
-}
-export const login = (req,res)=>{
-    res.send("login route")
-}
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
 
-export const logout = (req,res)=>{
-    res.send("logout route")
-}
+export const signup = async (req, res) => {
+    try {
+        const { fullName, email, password } = req.body;
 
+        if(!fullName || !email || !password){
+            return res.status(400).json({ msg: "All fields are required." });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ msg: "Password must be at least 6 characters long." });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: "User with this email already exists." });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        if (newUser) {
+            // Generate JWT token here
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({ message: "internal server error" });
+    }
+};
+
+export const login = (req, res) => {
+    res.send("login route");
+};
+
+export const logout = (req, res) => {
+    res.send("logout route");
+};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -13,25 +13,14 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-		username: {
-			type: String,
-			required: true,
-			unique: true,
-		},
+		
 		password: {
 			type: String,
 			required: true,
 			minlength: 6,
 		},
-		gender: {
-			type: String,
-			required: true,
-			enum: ["male", "female"],
-		},
-		profilePic: {
-			type: String,
-			default: "",
-		},
+		
+		
 		// createdAt, updatedAt => Member since <createdAt>
 	},
 	{ timestamps: true }
